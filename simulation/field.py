a6ebd8cc7358d875f3eb7c734f4f0c5f7b90b435
CharlesRobe <charles.robert1541@gmail.com>
2025-11-22 01:00:12 +0100

    TT - Modèle du terrain et du robot Fixes #29

---
+++ simulation/field.py
@@ -0,0 +1,50 @@
+import matplotlib.patches as patches
+import constants as c
+
+
+class EurobotField:
+    def __init__(self):
+        self.longueur = c.TABLE_LONGUEUR
+        self.largeur = c.TABLE_LARGEUR
+
+    def draw_static_elements(self, ax):
+        """Dessine les éléments fixes du plateau sur le graphique ax"""
+
+        # Configuration de l'affichage
+        ax.set_xlim(-200, self.longueur + 200)
+        ax.set_ylim(-200, self.largeur + 200)
+        ax.set_aspect('equal')
+
+        table = patches.Rectangle((0, 0), self.longueur, self.largeur,
+                                  facecolor=c.COLOR_FLOOR, edgecolor='black',
+                                  zorder=0)
+        ax.add_patch(table)
+
+        # Zone de départ BLEUE (Haut Gauche)
+        start_blue = patches.Rectangle((0,
+                                        self.largeur - c.START_ZONE_LONGUEUR),
+                                       c.START_ZONE_LARGEUR,
+                                       c.START_ZONE_LONGUEUR,
+                                       color=c.COLOR_BLUE, alpha=0.3,
+                                       label="Départ Bleu")
+        ax.add_patch(start_blue)
+
+        # Zone de départ JAUNE (Haut Droite)
+        start_yellow = patches.Rectangle((self.longueur - c.START_ZONE_LARGEUR,
+                                         self.largeur - c.START_ZONE_LONGUEUR),
+                                         c.START_ZONE_LARGEUR,
+                                         c.START_ZONE_LONGUEUR,
+                                         color=c.COLOR_YELLOW, alpha=0.3,
+                                         label="Départ Jaune")
+        ax.add_patch(start_yellow)
+
+        # Le Grenier
+        # Centré en X : (3000 - 1800) / 2 = 600
+        grenier_x = (self.longueur - c.GRENIER_LONGUEUR) / 2
+        grenier_y = self.largeur - c.GRENIER_LARGEUR
+
+        grenier = patches.Rectangle((grenier_x, grenier_y), c.GRENIER_LONGUEUR,
+                                    c.GRENIER_LARGEUR,
+                                    facecolor=c.COLOR_GRENIER, hatch='//',
+                                    label="Grenier", zorder=1)
+        ax.add_patch(grenier)
